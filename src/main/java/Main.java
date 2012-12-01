4b844327ae85aa85b5cc37ec681d6ac570814856
stephen <stephen.zambaux@gmail.com>
2012-12-01 07:55:45 +0100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import org.fuid.FuidRunner;
+
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		FuidRunner runner=new FuidRunner();
+		runner.run("rawOrganizer/src/main/META-INF/fuid.xml");
+
+	}
+
+}
